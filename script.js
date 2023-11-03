0eed20d64542de4afbe7dbb3b0ec0e341f4e5af3
HammadShinwari33 <hammadshinwari377@gmail.com>
2023-11-03 16:18:05 +0500

    Background-changer

---
+++ script.js
@@ -0,0 +1,19 @@
+function red(){
+    document.body.style.backgroundColor = "red";
+}
+
+function green(){
+    document.body.style.backgroundColor = "green";
+}
+
+function blue(){
+    document.body.style.backgroundColor = "blue";
+}
+
+function purple(){
+    document.body.style.backgroundColor = "purple";
+}
+
+function gray(){
+    document.body.style.backgroundColor = "gray";
+}
